0c37f26d314e58cb9139367420de877814977fef
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-11-26 14:40:18 -0300

    estrutura e dependências iniciais

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [require.resolve("babel-plugin-module-resolver"), {
+        root: ["."],
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ],
+        alias: {
+          "@components": "./src/components",
+          "@layouts": "./src/layouts",
+          "@routes": "./src/routes",
+          "@screens": "./src/screens",
+          "@interfaces": "./src/interfaces",
+          "@globalTypes": "./src/types",
+        }
+      }]
+    ]
   };
 };
