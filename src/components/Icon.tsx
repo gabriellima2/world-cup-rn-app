3f2d2e62ff7eef20ceb7d39f08444f3b53047450
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-11-26 15:41:33 -0300

    feat: adiciona icones na tab bar

---
+++ src/components/Icon.tsx
@@ -0,0 +1,4 @@
+import Ionicons from "react-native-vector-icons/MaterialCommunityIcons";
+import type { IconProps } from "react-native-vector-icons/Icon";
+
+export const Icon = (props: IconProps) => <Ionicons {...props} />;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,10 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import {
+	BottomTabNavigationOptions,
+	createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+
+import { Icon } from "@components/Icon";
 
 import { GroupScreen } from "@screens/GroupScreen";
 import { HomeScreen } from "@screens/HomeScreen";
@@ -7,16 +12,41 @@ import { TeamScreen } from "@screens/TeamScreen";
 
 const Tab = createBottomTabNavigator();
 
+function setRouteIcon(
+	iconName: string
+): Pick<BottomTabNavigationOptions, "tabBarIcon"> {
+	return {
+		tabBarIcon: ({ focused }) => (
+			<Icon name={iconName} size={24} color={focused ? "#c1c1c1" : "#111"} />
+		),
+	};
+}
 export const Routes = () => {
 	return (
 		<NavigationContainer independent>
 			<Tab.Navigator
 				initialRouteName="home"
-				screenOptions={{ headerShown: false }}
+				screenOptions={{
+					headerShown: false,
+					tabBarActiveTintColor: "#c1c1c1",
+					tabBarInactiveTintColor: "#111",
+				}}
 			>
-				<Tab.Screen name="home" component={HomeScreen} />
-				<Tab.Screen name="team" component={TeamScreen} />
-				<Tab.Screen name="group" component={GroupScreen} />
+				<Tab.Screen
+					name="home"
+					component={HomeScreen}
+					options={{ ...setRouteIcon("home"), title: "Início" }}
+				/>
+				<Tab.Screen
+					name="team"
+					component={TeamScreen}
+					options={{ ...setRouteIcon("soccer-field"), title: "Times" }}
+				/>
+				<Tab.Screen
+					name="group"
+					component={GroupScreen}
+					options={{ ...setRouteIcon("table"), title: "Grupos" }}
+				/>
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
